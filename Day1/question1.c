b17c5b33ab3b81ff2083b0d65d09480d0eba0938
SHASHANK-JOSHI21 <SHASHANK.40783@stu.upes.ac.in>
2026-08-31 21:56:35 +0530

    Organize Day 1 into Question 1 and Question 2

---
+++ Day1/question1.c
@@ -0,0 +1,25 @@
+//Q1: Write a program to input two numbers and display their sum.
+
+/*
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+#include <stdio.h>
+int main(){
+    float m1 , m2;
+    printf("Enter the number 1");
+    scanf("%f",&m1);
+    printf("Enter the number  2");
+    scanf("%f",&m2);
+    printf("total marks is =%f",m1+m2);
+      return 0;
+}

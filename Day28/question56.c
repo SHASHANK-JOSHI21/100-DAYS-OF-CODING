3acad4ae02d9bc981245e3c2d95ef465fd95965c
SHASHANK-JOSHI21 <SHASHANK.40783@stu.upes.ac.in>
2026-09-14 23:45:14 +0530

    Add Day28

---
+++ Day28/question56.c
@@ -0,0 +1,38 @@
+//Q56: Read and print elements of a one-dimensional array.
+
+/*
+Sample Test Cases:
+Input 1:
+3
+10 20 30
+Output 1:
+10 20 30
+
+Input 2:
+5
+1 2 3 4 5
+Output 2:
+1 2 3 4 5
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("Enter the size of array\n");
+    scanf("%d", &n);
+
+    int a[n];
+    printf("ENter the element of array\n");
+    // Input array elements
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Print array elements
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
